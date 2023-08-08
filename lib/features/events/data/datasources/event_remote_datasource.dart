fa59bbc64bfa5630a2ffeb4d45c56445743f05b8
Sanjar17 <sanjarsaidov173@gmail.com>
2023-08-08 11:52:19 +0500

    event datasource cretaed

---
+++ lib/features/events/data/datasources/event_remote_datasource.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:ticket_booking/features/events/data/models/event_model.dart';
+
+class EventRemoteDatasource {
+  Dio dio;
+
+  EventRemoteDatasource({required this.dio});
+
+  /// CReate a new event
+  Future<EventModel> createEvent(EventModel event) async {
+    final response = await dio.post(
+      '/events/create/',
+      data: event.toJson(),
+    );
+    return EventModel.fromJson(response.data);
+  }
+
+  /// Get all events
+  Future<List<EventModel>> getAllEvents() async {
+    final response = await dio.get('/events/all/');
+    return (response.data as List).map((e) => EventModel.fromJson(e)).toList();
+  }
+
+  /// Search events by name
+  Future<List<EventModel>> searchEvents(String query) async {
+    final response = await dio.get('/events/search/?search=$query');
+    return (response.data as List).map((e) => EventModel.fromJson(e)).toList();
+  }
+
+  /// update event by id
+  Future<EventModel> updateEvent(EventModel event) async {
+    final response = await dio.put(
+      '/events/{id}/${event.id}/update/',
+      data: event.toJson(),
+    );
+    return EventModel.fromJson(response.data);
+  }
+
+  /// delete event by id
+  Future<void> deleteEvent(String id) async {
+    await dio.delete('/events/$id/delete/');
+  }
+}
---
+++ lib/features/events/data/models/event_model.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: constant_identifier_names
+
+enum Currency { USD, UZS, PLN, RUB }
+
+// currency as map
+const currencyMap = {
+  Currency.USD: 'USD',
+  Currency.UZS: 'UZS',
+  Currency.PLN: 'PLN',
+  Currency.RUB: 'RUB',
+};
+
+class EventModel {
+  final String? id;
+  final String name;
+  final String topic;
+  final DateTime date;
+  final String place;
+  final int numberOfSeats;
+  final String ticketPrice;
+  final Currency currency;
+  final String thumbnail;
+  final String description;
+
+  EventModel({
+    this.id,
+    required this.name,
+    required this.topic,
+    required this.date,
+    required this.place,
+    required this.numberOfSeats,
+    required this.ticketPrice,
+    required this.currency,
+    required this.thumbnail,
+    required this.description,
+  });
+
+  factory EventModel.fromJson(Map<String, dynamic> json) {
+    return EventModel(
+      id: json['id'],
+      name: json['name'],
+      topic: json['topic'],
+      date: DateTime.parse(json['date']),
+      place: json['place'],
+      numberOfSeats: json['number_of_seats'],
+      ticketPrice: json['ticket_price'],
+      currency: currencyMap.keys.firstWhere((element) => currencyMap[element] == json['currency']),
+      thumbnail: json['thumbnail'],
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id ?? '1',
+      'name': name,
+      'topic': topic,
+      'date': date.toIso8601String(),
+      'place': place,
+      'number_of_seats': numberOfSeats,
+      'ticket_price': ticketPrice,
+      'currency': currencyMap[currency],
+      'thumbnail': thumbnail,
+      'description': description,
+    };
+  }
+}
